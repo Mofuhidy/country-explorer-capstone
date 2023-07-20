6d3aed865b675b42aefe6504f849f07d42bfb488
moFuhidy <mo7ammed9290@gmail.com>
2023-07-20 21:13:04 +0300

    Add pages and files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CountryDetails from './pages/CountryDetails';
+
 function App() {
   return (
-    <div />
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/conutrydata/:countryId" element={<CountryDetails />} />
+      </Routes>
+    </>
   );
 }
 
